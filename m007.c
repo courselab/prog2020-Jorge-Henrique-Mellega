2d3dc08423fdde69fd5c858b653c4f8a007355f8
Jorge-Henrique-Mellega <63014855+Jorge-Henrique-Mellega@users.noreply.github.com>
2020-07-07 23:01:49 -0300

    Update m007.c

--- m007.c
+++ m007.c
@@ -26,9 +26,18 @@
 
 /* Count the number of letters in string s.*/
 
-int lettercount (char *s)
-{
-  return 0;
+int lettercount(char* s){
+    int c=0, nC=0, i;
+    while(s[c]!='\0'){
+        i = (int)s[c];
+        if(i > 64 && i < 91){
+            nC++;
+        }else if ( i >  96 && i < 123){
+            nC++;
+        }
+        c++;
+    }
+    return nC;
 }
 
 
